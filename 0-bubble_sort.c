7f8416076fbbc72a908639b0581426046a73177e
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-11-19 15:07:15 +0100

    task 0 fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,17 +22,15 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
+				check = 1;
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
-				check = 1;
 			}
 		}
 
 		if (check == 0)
-		{
 			break;
-		}
 	}
 }
